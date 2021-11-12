adf4d10cff24d7f718b965f7df7607620537d325
Edward Son <Edwardhson@gmail.com>
2021-11-11 20:05:53 -0800

    Initial commit

---
+++ Calculator/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 0; int num2 = 0;
+
+            Console.WriteLine("Calulator in C#\r");
+            Console.WriteLine("----------------\n");
+
+            Console.WriteLine("Type a number, and then press Enter");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Type another number, and then press Enter");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Choose an option from the following list:");
+            Console.WriteLine("\ta - Add");
+            Console.WriteLine("\ts - Subtract");
+            Console.WriteLine("\tm - Multiply");
+            Console.WriteLine("\td - Divide");
+            Console.Write("Please choose an option: ");
+            
+            switch (Console.ReadLine())
+            {
+                case "a":
+                    Console.WriteLine($"The Answer is: {num1} + {num2} = " + (num1 + num2));
+                    break;
+                case "s":
+                    Console.WriteLine($"Your result: {num1} - {num2} = " + (num1 - num2));
+                    break;
+                case "m":
+                    Console.WriteLine($"Your result: { num1} * { num2} = " + (num1 * num2));
+                    break;
+                case "d":
+                    while(num2 == 0)
+                    {
+                        Console.WriteLine("Cannot compute, please enter a non-zero divisor: ");
+                        num2 = Convert.ToInt32(Console.ReadLine());
+                    }
+                    Console.WriteLine($"Your result: {num1} / {num2} = " + (num1 / num2));
+                    break;
+            }
+            Console.Write("Press any key to exit");
+                Console.ReadKey();
+        }
+    }
+}
